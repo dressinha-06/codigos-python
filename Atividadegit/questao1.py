13dcdebe1ee55daffdbad68ab075f1bed8f7b597
Andressa_Lara <andressaacgomes@gmail.com>
2024-09-03 17:57:06 -0300

    adicionado duas questoes

---
+++ Atividadegit/questao1.py
@@ -0,0 +1,7 @@
+idade = int(input("Informe a sua idade: "))
+
+idade1 = (idade*365)
+
+idade2 = (idade*12)
+
+print(f"Sua idade em dias é {idade1} e sua idade em meses é {idade2}")
---
+++ Atividadegit/questao3.py
@@ -0,0 +1,9 @@
+
+
+nome = (input("nome da fazenda: "))
+ttlcavalo = int (input("total de cavalos: "))
+
+qtdferradura = (ttlcavalo*4)
+
+print(f"A quantidade de farraduras é de {qtdferradura}")
+
